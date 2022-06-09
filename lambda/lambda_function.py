7041b2536d9998ea16b7c85e0055f6b48c1466b5
Matt Lisle <pronouncedlyle@gmail.com>
2022-06-09 16:46:09 -0400

    Add lambda function, ddb table, and connected them

---
+++ lambda/lambda_function.py
@@ -0,0 +1,23 @@
+import os
+import boto3
+
+# get the service resource
+dynamoddb = boto3.resource('dynamodb')
+
+# set environment variable
+TABLE_NAME = os.environ['TABLE_NAME']
+
+def lambda_handler(event, context):
+    table = dynamoddb.Table(TABLE_NAME)
+    # put item in table
+    response = table.put_item(
+        Item={
+            'id': str("Hi")
+        }
+    )
+
+    print("PutItem succeeded:")
+
+    return {
+        'statusCode': 200,
+    }
--- speed_test/speed_test_stack.py
+++ speed_test/speed_test_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda,
+    aws_dynamodb,
 )
 from constructs import Construct
 
@@ -10,10 +10,23 @@ class SpeedTestStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # create dynamoDB table
+        demo_table = aws_dynamodb.Table(
+            self, "demo_table",
+            partition_key=aws_dynamodb.Attribute(
+                name="id",
+                type=aws_dynamodb.AttributeType.STRING
+            )
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "SpeedTestQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # create lambda function
+        lambda_function = aws_lambda.Function(
+            self, "lambda_function",
+            runtime=aws_lambda.Runtime.PYTHON_3_7,
+            handler="lambda_function.lambda_handler",
+            code=aws_lambda.Code.from_asset("./lambda")
+        )
+        lambda_function.add_environment("TABLE_NAME", demo_table.table_name)
+
+        # grant permission to lambda to write to demo table
+        demo_table.grant_write_data(lambda_function)
