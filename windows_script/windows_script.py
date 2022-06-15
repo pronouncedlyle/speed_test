8658db4f8f8a71c0eabe426f111d171267987c2a
Matt Lisle <pronouncedlyle@gmail.com>
2022-06-15 14:26:50 -0400

    API call

--- windows_script/windows_script.py
+++ windows_script/windows_script.py
@@ -1,4 +1,5 @@
 import subprocess
+import requests 
 from sys import stdout
 
 #executes cd to the specified file. TODO: Do I need to do the cd first? This doesn't transfer well
@@ -16,6 +17,10 @@ print(output2.stdout)
 
 # command to execute: speedtest-cli --json
 
-#TODO: grab relevant data from speedtest json results. Figure out which data type would be easiest to send to dynamodb https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Programming.LowLevelAPI.html
+#TODO: grab relevant data from speedtest json results. Make variable or pare down JSON to relevant info. https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Programming.LowLevelAPI.html
 
 #TODO: write a post method to API gateway https://github.com/aws-samples/aws-cdk-examples/tree/master/python/api-sqs-lambda
+# https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+# http endpoint for API GW. Need to limit access somehow. https://dpjwnub3z5.execute-api.us-east-1.amazonaws.com/prod/%7Bproxy+%7D
+apiurl= 'https://dpjwnub3z5.execute-api.us-east-1.amazonaws.com/prod/{proxy+}'
+requests.post(apiurl) # add json={key: value} and other needed args later. 
